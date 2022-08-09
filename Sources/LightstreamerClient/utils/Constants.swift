a12232d1dc58a10d693843baff82aaed592123bf
acarioni <alessandro.carioni@lightstreamer.com>
2022-08-09 14:36:47 +0200

    update version number

--- Sources/LightstreamerClient/utils/Constants.swift
+++ Sources/LightstreamerClient/utils/Constants.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 #if os(macOS)
-var LS_CID = "scFuxkwp1ltvcB4BJ4JikvD9i"
+var LS_CID = "scFuxkwp1ltvcB4BJ4JikvDAj"
 #elseif os(tvOS)
-var LS_CID = "zxRyen2m uz.k55AK1xlzcim"
+var LS_CID = "zxRyen2m uz.k55AK1xlzcjn"
 #elseif os(watchOS)
-var LS_CID = ".cWimz9dysogQz2GJ4L73dHzfEn"
+var LS_CID = ".cWimz9dysogQz2GJ4L73dHzfFo"
 #else
-var LS_CID = "oqVfhw.i6 37e64BHf  g5i"
+var LS_CID = "oqVfhw.i6 37e64BHf  g6j"
 #endif
 let LS_LIB_NAME = "swift_client"
-let LS_LIB_VERSION = "5.0.0 beta3"
+let LS_LIB_VERSION = "5.0.0 beta4"
 
 var TLCP_VERSION = "TLCP-2.3.0"
 let FULL_TLCP_VERSION = TLCP_VERSION + ".lightstreamer.com"
