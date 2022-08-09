ca3ddd8ba2997516f927d32844076417b1ace35f
acarioni <alessandro.carioni@lightstreamer.com>
2022-08-09 12:02:03 +0200

    adjust podspec

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        // NB keep in sync with LightstreamerClient.podspec
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.4")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
     ],
