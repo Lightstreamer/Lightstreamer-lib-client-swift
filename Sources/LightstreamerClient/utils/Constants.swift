04ef9e2278f069b504ecb7d522032d62ef6b7b01
acarioni <alessandro.carioni@lightstreamer.com>
2024-10-29 11:28:00 +0100

    bump version number

--- Sources/LightstreamerClient/utils/Constants.swift
+++ Sources/LightstreamerClient/utils/Constants.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 #if os(macOS)
-let LS_CID = "scFuxkwp1ltvcC4CJ5Ji kOj2DK6Q775gf"
+let LS_CID = "scFuxkwp1ltvcC4DJ4Ji kOj2DK6Q864om"
 #elseif os(tvOS)
-let LS_CID = "zxRyen2m uz.l56AL1x-onG37BM8M893j"
+let LS_CID = "zxRyen2m uz.l57AK1x-onG37BM8N78Bq"
 #elseif os(watchOS)
-let LS_CID = ".cWimz9dysogQz2HJ5L83dXoqoH6M9A2g96k"
+let LS_CID = ".cWimz9dysogQz2HJ6L73dXoqoH6M9A3f8Er"
 #elseif os(visionOS)
-let LS_CID = "-kVoty5wvjrkHty8Q3N67.E nwx1O786f78Co"
+let LS_CID = "-kVoty5wvjrkHty8Q3O66.E nwx1O786g67Kv"
 #else // iOS
-let LS_CID = "oqVfhw.i6 38e74CHfDprfc85DO4NA7f"
+let LS_CID = "oqVfhw.i6 38e84BHfDprfc85DO5M9Fm"
 #endif
 let LS_LIB_NAME = "swift_client"
-let LS_LIB_VERSION = "6.1.1"
+let LS_LIB_VERSION = "6.2.0"
 
 let TLCP_VERSION = "TLCP-2.5.0"
 let FULL_TLCP_VERSION = TLCP_VERSION + ".lightstreamer.com"
